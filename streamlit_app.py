53826486eaab2c413c5234e5ffb6e03431e6750c
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 17:38:15 -0600

    Update streamlit_app.py
    nutrition code

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ for fruit_chosen in ingredients_list:
      #   search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
      #   st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-     #   st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 #st.text(ingredients_string)
