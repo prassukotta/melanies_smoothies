70f23a349e08e6305a22ebbb4d43b2be2afcbacb
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 17:02:44 -0600

    Update streamlit_app.py
    2ns

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ st.text(ingredients_string)
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
+st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button('Submit Order')
     
