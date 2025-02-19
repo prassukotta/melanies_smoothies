c8724f9f7741bb8ba5f0d33e469d1c66962fc2ea
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 16:41:19 -0600

    Update streamlit_app.py
    gng

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,7 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_or
 
    # st.write(my_insert_stmt)
    # #st.stop()
-    time_to_insert = st.button('Submit Order')
-    
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-
-        st.success('Your Smoothie is ordered!')
+time_to_insert = st.button('Submit Order')
+     if time_to_insert:
+         session.sql(my_insert_stmt).collect()
+         st.success('Your Smoothie is ordered!')
