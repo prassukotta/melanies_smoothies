362d6ffa2e1cb932e7fa6148bc42db6ffd8b73a3
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 16:09:18 -0600

    Update streamlit_app.py
    F

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
-
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -20,15 +19,14 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect
-(
- 'Choose up to 5 ingredients:'
+     'Choose up to 5 ingredients:'
      , my_dataframe
  )
 
 if ingredients_list:
     ingredients_string= ''
 
- for fruit_chosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
