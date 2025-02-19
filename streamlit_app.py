ca731cc15c2773ec1e80278f3d9e0e7a5abadbf8
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 16:06:40 -0600

    Update streamlit_app.py
    P

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ ingredients_list = st.multiselect
 if ingredients_list:
     ingredients_string= ''
 
-    for fruit_chosen in ingredients_list:
+ for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
