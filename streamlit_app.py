5f4009ab9e38bd3cb553184a4c6da4c2e763a43a
prassukotta <48070242+prassukotta@users.noreply.github.com>
2025-02-19 19:57:16 -0600

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,13 +39,13 @@ for fruit_chosen in ingredients_list:
  #st.text(ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-      values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+       values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 st.write(my_insert_stmt)
     #st.stop()
 time_to_insert = st.button('Submit Order')
     
 if time_to_insert:
-    session.sql(my_insert_stmt).collect()
+     session.sql(my_insert_stmt).collect()
 
-    st.success('Your Smoothie is ordered!')
+     st.success('Your Smoothie is ordered!')
